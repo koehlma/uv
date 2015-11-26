aa29957a1a847edafb7fab82f8c308fb31ec92e6
Maximilian Köhl <mail@koehlma.de>
2015-11-26 23:38:47 +0100

    fix rtd

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,12 +39,7 @@ exclude_patterns = []
 
 pygments_style = 'sphinx'
 
-todo_include_todos = True
-
-import sphinx_rtd_theme
-
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+todo_include_todos = False
 
 html_static_path = ['_static']
 
