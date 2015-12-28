a5b9b861c4615f0c6f47d42e0a658fac2ffc2c2b
Maximilian Köhl <mail@koehlma.de>
2015-12-28 15:15:04 +0100

    LGPLv3+ → LGPLv3

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2015, Maximilian Köhl <mail@koehlma.de>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import re
 import sys
--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
+
 # Copyright (C) 2015, Maximilian Köhl <mail@koehlma.de>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3 as published by
+# the Free Software Foundation.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import os.path
@@ -59,8 +58,7 @@ _path_2 = os.path.expandvars(r'%SYSTEMDRIVE%\Python27\python.exe')
 WIN32_PYTHON27_PATHS = [_path_1, _path_2]
 
 
-LICENSE = ('License :: OSI Approved :: GNU Lesser General Public '
-           'License v3 or later (LGPLv3+)')
+LICENSE = 'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
 
 
 with open(os.path.join(__dir__, 'uv', '__init__.py'), 'rb') as init_py:
