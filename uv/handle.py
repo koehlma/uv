75fd9d1c3b5dfa11bb82194f4d00b42b4e142aa0
Maximilian Köhl <mail@koehlma.de>
2016-01-23 20:57:12 +0100

    handle: update documentation

--- uv/handle.py
+++ uv/handle.py
@@ -15,8 +15,6 @@
 
 from __future__ import print_function, unicode_literals, division, absolute_import
 
-import threading
-
 from . import common, error, library
 from .library import ffi, lib
 from .loop import Loop
@@ -70,20 +68,21 @@ def uv_close_cb(uv_handle):
     handle.set_closed()
 
 
+# the following code is considered free of data races because of the
+# GIL; if we introduce locking here this might deadlock because the
+# GC could perform a collection cycle within the with statement
+# TODO: this is a really nasty and ugly hack of course
 _finalizing = set()
-_finalizing_lock = threading.RLock()
 
 
 @ffi.callback('uv_close_cb')
 def uv_close_cb_finalize(uv_handle):
-    with _finalizing_lock:
-        _finalizing.remove(uv_handle)
+    _finalizing.remove(uv_handle)
 
 
 def handle_finalizer(uv_handle):
-    with _finalizing_lock:
-        # do not garbage collect the handle until it has been closed
-        _finalizing.add(uv_handle)
+    # do not garbage collect the handle until it has been closed
+    _finalizing.add(uv_handle)
     # remove attached garbage collected handle
     uv_handle.data = ffi.NULL
     # TODO: this might lead to data races
@@ -95,8 +94,8 @@ def handle_finalizer(uv_handle):
 class Handle(object):
     """
     Handles represent long-lived objects capable of performing certain
-    operations while active. This is the base class of all handles except
-    the file and SSL handle, which are pure Python.
+    operations while active. This is the base class of all handles
+    except the file and SSL handle, which are pure Python.
 
     .. note::
         Handles underlie a special garbage collection strategy which
@@ -105,208 +104,296 @@ class Handle(object):
         calling a callback it is excluded from garbage collection as
         well as the corresponding event loop.
 
-    :raises uv.LoopClosedError: loop has already been closed
+    :raises uv.LoopClosedError:
+        loop has already been closed
 
-    :param uv_handle: allocated c struct for this handle
-    :param loop: loop where the handle should run on
+    :param uv_handle:
+        allocated c struct for this handle (with CFFI ownership)
+    :param loop:
+        loop where the handle should run on
 
-    :type uv_handle: ffi.CData
-    :type loop: Loop
+    :type uv_handle:
+        ffi.CData
+    :type loop:
+        uv.Loop
     """
 
-    __slots__ = ['__weakref__', 'uv_handle', 'attachment', 'loop', 'on_closed',
+    __slots__ = ['__weakref__', 'loop', 'uv_handle', 'attachment', 'on_closed',
                  'closed', 'closing', 'data', 'allocator']
 
     def __init__(self, uv_handle, loop=None):
+        self.loop = loop or Loop.get_current()
+        if self.loop.closed:
+            raise error.ClosedLoopError()
         self.uv_handle = ffi.cast('uv_handle_t*', uv_handle)
         self.attachment = library.attach(self.uv_handle, self)
-        self.loop = loop or Loop.get_current()
         """
-        Loop where the handle is running on.
+        Loop the handle is running on.
 
-        :readonly: True
-        :type: Loop
+        :readonly:
+            True
+        :type:
+            uv.Loop
         """
         self.on_closed = common.dummy_callback
         """
-        Callback which should be called after the handle has been closed.
+        Callback which should run after the handle has been closed.
 
-        .. function:: on_closed(Handle)
 
-        :readonly: False
-        :type: (uv.Handle) -> None
+        .. function:: on_closed(handle)
+
+            :param handle:
+                handle which has been closed
+
+            :type handle:
+                uv.Handle
+
+
+        :readonly:
+            False
+        :type:
+            ((uv.Handle) -> None) | ((Any, uv.Handle) -> None)
         """
         self.closed = False
         """
-        Handle has been closed. This is `True` right after the close callback
-        has been called. It means all internal resources are freed and this
-        handle is ready to be garbage collected.
+        Handle has been closed. This is `True` right after the close
+        callback has been called. It means all internal resources are
+        freed and this handle is ready to be garbage collected.
 
-        :readonly: True
-        :type: bool
+        :readonly:
+            True
+        :type:
+            bool
         """
         self.closing = False
         """
-        Handle is already closed or is closing. This is `True` right after
-        close has been called. Operations on a closed or closing handle will
-        raise :class:`uv.HandleClosedError`.
+        Handle is already closed or is closing. This is `True` right
+        after close has been called. Operations on a closed or closing
+        handle will raise :class:`uv.HandleClosedError`.
 
-        :readonly: True
-        :type: bool
+        :readonly:
+            True
+        :type:
+            bool
         """
         self.data = None
         """
-        User-specific data of any type (necessary because we are using slots).
+        User-specific data of any type. This is necessary because of
+        the usage of slots.
 
-        :readonly: False
+        :readonly:
+            False
+        :type:
+            Any
         """
-        if self.loop.closed: raise error.ClosedLoopError()
         self.allocator = self.loop.allocator
+        """
+        Allocator used to allocate new read buffers for this handle.
+
+        :readonly:
+            False
+        :type:
+            uv.loop.Allocator
+        """
         common.attach_finalizer(self, handle_finalizer, uv_handle)
 
     @property
     def active(self):
         """
-        Handle is active or not. What "active" means depends on the handle:
+        Handle is active or not. What "active" means depends on the
+        handle type:
 
-        - :class:`uv.Async`: is always active and cannot be deactivated
+        - :class:`uv.Async`:
+            is always active and cannot be deactivated
 
-        - :class:`uv.Pipe`, :class:`uv.TCP`, :class:`uv.UDP`, ...: basically
-          any handle dealing with IO is active when it is doing something
-          involves IO like reading, writing, connecting or listening
+        - :class:`uv.Pipe`, :class:`uv.TCP`, :class:`uv.UDP`, …:
+            basically any handle dealing with IO is active when it is
+            doing something involves IO like reading, writing,
+            connecting or listening
 
-        - :class:`uv.Check`, :class:`uv.Idle`, :class:`uv.Timer`, ...: handle
-          is active when it has been started and not yet stopped
+        - :class:`uv.Check`, :class:`uv.Idle`, :class:`uv.Timer`, …:
+            handle is active when it has been started and not yet
+            stopped
 
-        :readonly: True
-        :type: bool
+        :readonly:
+            True
+        :type:
+            bool
         """
-        if self.closed: return False
+        if self.closed:
+            return False
         return bool(lib.uv_is_active(self.uv_handle))
 
     @property
     def referenced(self):
         """
-        Handle is referenced or not. If the event loop runs in default mode it
-        will exit when there are no more active and referenced handles left. This
-        has nothing to do with CPython's reference counting.
+        Handle is referenced or not. If the event loop runs in default
+        mode it will exit when there are no more active and referenced
+        handles left. This has nothing to do with CPython's reference
+        counting.
 
-        :readonly: False
-        :type: bool
+        :readonly:
+            False
+        :type:
+            bool
         """
-        if self.closed: return False
+        if self.closed:
+            return False
         return bool(lib.uv_has_ref(self.uv_handle))
 
     @referenced.setter
     def referenced(self, referenced):
         """
-        :param referenced: referenced status which should be set
-        :type referenced: bool
+        :param referenced:
+            referenced status which should be set
+        :type referenced:
+            bool
         """
-        if referenced: self.reference()
-        else: self.dereference()
+        if referenced:
+            self.reference()
+        else:
+            self.dereference()
 
     @property
     def send_buffer_size(self):
         """
         Size of the send buffer that the operating system uses for the
-        socket. The following handles are supported: TCP and UDP handles
-        on Unix and Windows, Pipe handles only on Unix. On all unsupported
-        handles this will raise :class:`uv.UVError` with `StatusCode.EINVAL`.
+        socket. The following handles are supported: TCP and UDP
+        handles on Unix and Windows, Pipe handles only on Unix. On all
+        unsupported handles this will raise :class:`uv.UVError` with
+        error code `EINVAL` (:class:`uv.error.InvalidTypeError`).
 
         .. note::
+            Unlike libuv this library abstracts the different
+            behaviours on Linux and other operating systems. This
+            means, the size set is divided by two on Linux because
+            Linux internally multiplies it by two.
 
-            Unlike libuv this library abstracts the different behaviours on Linux
-            and other operating systems. This means, the size set is divided by
-            two on Linux because Linux internally multiplies it by two.
+        :raises uv.UVError:
+            error while getting/setting the send buffer size
+        :raises uv.HandleClosedError:
+            handle has already been closed or is closing
 
-        :raises uv.UVError: error while getting/setting the send buffer size
-        :raises uv.HandleClosedError: handle has already been closed or is closing
-        :readonly: False
-        :type: int
+        :readonly:
+            False
+        :type:
+            int
         """
-        if self.closing: raise error.ClosedHandleError()
+        if self.closing:
+            raise error.ClosedHandleError()
         c_buffer_size = ffi.new('int*')
         code = lib.uv_send_buffer_size(self.uv_handle, c_buffer_size)
-        if code < 0: raise error.UVError(code)
+        if code < 0:
+            raise error.UVError(code)
         return c_buffer_size[0]
 
     @send_buffer_size.setter
     def send_buffer_size(self, size):
         """
-        :raises uv.UVError: error while getting/setting the send buffer size
-        :raises uv.HandleClosedError: handle has already been closed or is closing
-        :param size: size of the send buffer
-        :type size: int
+        :raises uv.UVError:
+            error while getting/setting the send buffer size
+        :raises uv.HandleClosedError:
+            handle has already been closed or is closing
+
+        :param size:
+            size of the send buffer
+
+        :type size:
+            int
         """
-        if self.closing: raise error.ClosedHandleError()
+        if self.closing:
+            raise error.ClosedHandleError()
         c_buffer_size = ffi.new('int*', int(size / 2) if is_linux else size)
         code = lib.uv_send_buffer_size(self.uv_handle, c_buffer_size)
-        if code < 0: raise error.UVError(code)
+        if code < 0:
+            raise error.UVError(code)
 
     @property
     def receive_buffer_size(self):
         """
-        Size of the receive buffer that the operating system uses for the
-        socket. The following handles are supported: TCP and UDP handles
-        on Unix and Windows, Pipe handles only on Unix. On all unsupported
-        handles this will raise :class:`uv.UVError` with `StatusCode.EINVAL`.
+        Size of the receive buffer that the operating system uses for
+        the socket. The following handles are supported: TCP and UDP
+        handles on Unix and Windows, Pipe handles only on Unix. On all
+        unsupported handles this will raise :class:`uv.UVError` with
+        error code `EINVAL` (:class:`uv.error.InvalidTypeError`).
 
         .. note::
+            Unlike libuv this library abstracts the different
+            behaviours on Linux and other operating systems. This
+            means, the size set is divided by two on Linux because
+            Linux internally multiplies it by two.
 
-            Unlike libuv this library abstracts the different behaviours on Linux
-            and other operating systems. This means, the size set is divided by
-            two on Linux because Linux internally multiplies it by two.
+        :raises uv.UVError:
+            error while getting/setting the receive buffer size
+        :raises uv.HandleClosedError:
+            handle has already been closed or is closing
 
-        :raises uv.UVError: error while getting/setting the receive buffer size
-        :raises uv.HandleClosedError: handle has already been closed or is closing
-        :readonly: False
-        :type: int
+        :readonly:
+            False
+        :type:
+            int
         """
-        if self.closing: raise error.ClosedHandleError()
+        if self.closing:
+            raise error.ClosedHandleError()
         c_buffer_size = ffi.new('int*')
         code = lib.uv_recv_buffer_size(self.uv_handle, c_buffer_size)
-        if code < 0: raise error.UVError(code)
+        if code < 0:
+            raise error.UVError(code)
         return c_buffer_size[0]
 
     @receive_buffer_size.setter
     def receive_buffer_size(self, size):
         """
-        :raises uv.UVError: error while getting/setting the receive buffer size
-        :raises uv.HandleClosedError: handle has already been closed or is closing
-        :param size: size of the receive buffer
-        :type size: int
+        :raises uv.UVError:
+            error while getting/setting the receive buffer size
+        :raises uv.HandleClosedError:
+            handle has already been closed or is closing
+
+        :param size:
+            size of the receive buffer
+
+        :type size:
+            int
         """
-        if self.closing: raise error.ClosedHandleError()
+        if self.closing:
+            raise error.ClosedHandleError()
         c_buffer_size = ffi.new('int*', int(size / 2) if is_linux else size)
         code = lib.uv_recv_buffer_size(self.uv_handle, c_buffer_size)
-        if code < 0: raise error.UVError(code)
+        if code < 0:
+            raise error.UVError(code)
 
     def fileno(self):
         """
-        Gets the platform dependent file descriptor equivalent. The
+        Get the platform dependent file descriptor equivalent. The
         following handles are supported: TCP, UDP, TTY, Pipes and Poll.
         On all other handles this will raise :class:`uv.UVError` with
-        error code `EINVAL`.
+        error code `EINVAL` (:class:`uv.error.InvalidTypeError`).
 
-        If a handle does not have an attached file descriptor yet this method
-        will raise :class:`uv.UVError` with `StatusCode.EBADF`.
+        If a handle does not have an attached file descriptor yet this
+        method will raise :class:`uv.UVError` with error code `EBADF`
+        (:class:`uv.error.BadFileDescriptorError`).
 
         .. warning::
+            Be very careful when using this method. Libuv assumes it is
+            in control of the file descriptor so any change to it may
+            result in unpredictable malfunctions.
 
-            Be very careful when using this method. Libuv assumes it
-            is in control of the file descriptor so any change to it
-            may result in unpredictable malfunctions.
+        :raises uv.UVError:
+            error while receiving fileno
+        :raises uv.HandleClosedError:
+            handle has already been closed or is closing
 
-        :raises uv.UVError: error while receiving fileno
-        :raises uv.HandleClosedError: handle has already been closed or is closing
-        :return: platform dependent file descriptor equivalent
-        :rtype: int
+        :return:
+            platform dependent file descriptor equivalent
+        :rtype:
+            int
         """
-        if self.closing: raise error.ClosedHandleError()
+        if self.closing:
+            raise error.ClosedHandleError()
         uv_fd = ffi.new('uv_os_fd_t*')
         code = lib.uv_fileno(self.uv_handle, uv_fd)
-        if code < 0: raise error.UVError(code)
+        if code < 0:
+            raise error.UVError(code)
         return ffi.cast('int*', uv_fd)[0]
 
     def reference(self):
@@ -320,7 +407,8 @@ class Handle(object):
         :raises uv.HandleClosedError:
             handle has already been closed or is closing
         """
-        if self.closing: raise error.ClosedHandleError()
+        if self.closing:
+            raise error.ClosedHandleError()
         lib.uv_ref(self.uv_handle)
 
     def dereference(self):
@@ -334,19 +422,20 @@ class Handle(object):
         :raises uv.HandleClosedError:
             handle has already been closed or is closing
         """
-        if self.closing: raise error.ClosedHandleError()
+        if self.closing:
+            raise error.ClosedHandleError()
         lib.uv_unref(self.uv_handle)
 
     def close(self, on_closed=None):
         """
         Close the handle. Please make sure to call this method on any
         handle you do not need anymore. This method is idempotent, that
-        is, if the handle is already closed or is closing calling this
-        method will have no effect at all.
+        is, if the handle is already closed or is closing calling it
+        will have no effect at all.
 
         In-progress requests, like connect or write requests, are
         cancelled and have their callbacks called asynchronously with
-        :class:`StatusCode.ECANCELED`.
+        :class:`uv.StatusCodes.ECANCELED`.
 
         After this method has been called on a handle no operations can
         be performed on it (they raise :class:`uv.HandleClosedError`).
@@ -364,12 +453,13 @@ class Handle(object):
         :type on_closed:
             ((uv.Handle) -> None) | ((Any, uv.Handle) -> None)
         """
-        if self.closing: return
+        if self.closing:
+            return
         self.closing = True
         self.on_closed = on_closed or self.on_closed
-        # exclude the handle form garbage collection until it is closed
+        # exclude the handle from garbage collection until it is closed
         self.gc_exclude()
-        # the garbage collection does not have to close the handle anymore
+        # the finalizer does not have to close the handle anymore
         common.detach_finalizer(self)
         lib.uv_close(self.uv_handle, uv_close_cb)
 
@@ -391,7 +481,7 @@ class Handle(object):
             This method is only for internal purposes and is not part
             of the official API. It deactivates the garbage collection
             for the handle which means the handle and the corresponding
-            loop are excluded form garbage collection. You should never
+            loop are excluded from garbage collection. You should never
             call it directly!
         """
         self.loop.gc_exclude_structure(self)
